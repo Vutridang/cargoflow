0cbff0aba15d3918ec17d0066c276bbda96b992a
Vutridang <dangvu8244@gmail.com>
2026-08-14 16:12:26 +0700

    chore: configure MongoDB validation and Swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      // Make configuration variables available throughout the application
+      // without importing ConfigModule in every feature module.
+      isGlobal: true,
+    }),
+
+    MongooseModule.forRoot(process.env.MONGO_URI as string),
+  ],
 })
 export class AppModule {}
