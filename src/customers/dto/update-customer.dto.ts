d849d157125f6591ec64e5d8ccdba9b94fc0e83c
Vutridang <dangvu8244@gmail.com>
2026-09-25 16:02:59 +0700

    refactor: prevent updating customer userId

--- src/customers/dto/update-customer.dto.ts
+++ src/customers/dto/update-customer.dto.ts
@@ -4,5 +4,5 @@ import { CreateCustomerDto } from './create-customer.dto';
 // Make all remaining fields optional
 export class UpdateCustomerDto extends PartialType(
   // Exclude customerCode from the update DTO
-  OmitType(CreateCustomerDto, ['customerCode'] as const),
+  OmitType(CreateCustomerDto, ['customerCode', 'userId'] as const),
 ) {}
